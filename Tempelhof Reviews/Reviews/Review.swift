0ed38ca0247198cdece1a0f35fec7f2d03db5388
Furqan Khan <furqan@freeletics.com>
2019-05-24 22:10:29 +0200

    Added network call and Review model

--- Tempelhof Reviews/Reviews/Review.swift
+++ Tempelhof Reviews/Reviews/Review.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+enum TravelerType: String, Decodable {
+    case couple, solo, friends, familyYoung
+}
+
+struct Review: Decodable {
+    let reviewId: Int
+    let rating: Double
+    let title: String?
+    let message: String?
+    let author: String
+    let foreignLanguage: Bool
+    let date: Date
+    let languageCode: String
+    let travelerType: TravelerType?
+    let reviewerName: String
+    let reviewerCountry: String
+    let reviewerProfilePhoto: String?
+    let isAnonymous: Bool
+    let firstInitial: String
+}
+
+struct ReviewRequestResponse: Decodable {
+    let data: [Review]
+}
--- Tempelhof Reviews/Reviews/ReviewsDataProvider.swift
+++ Tempelhof Reviews/Reviews/ReviewsDataProvider.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+final class ReviewsDataProvider {
+    
+    enum Result {
+        case sucess([Review])
+        case failure(Error)
+    }
+    
+    enum FetchError: Error {
+        case invalidUrl
+        case noData
+        case parsingFailed
+    }
+    
+    let host = "www.getyourguide.com"
+    let urlComponents = ["berlin-l17", "tempelhof-2-hour-airport-history-tour-berlin-airlift-more-t23776", "reviews.json"]
+    
+    /// Requests reviews.
+    ///
+    /// - Parameter onComplete: call back with a list of reviews.
+    func fetchReviews(count: Int, page: Int, onComplete: @escaping (Result)->Void) {
+        
+        let urlString = (["https://" + host] + urlComponents).joined(separator: "/")
+        guard let url = URL(string: urlString) else {
+            onComplete(.failure(FetchError.invalidUrl))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            // Complete with generic error from response.
+            if let error = error {
+                onComplete(.failure(error))
+                return
+            }
+            
+            // Complete with error on data-less response.
+            guard let data = data else {
+                onComplete(.failure(FetchError.noData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            // Attempt decoding response.
+            guard let reviewRequestResponse = try? decoder.decode(ReviewRequestResponse.self, from: data) else {
+                onComplete(.failure(FetchError.parsingFailed))
+                return
+            }
+            
+            // Complete with successful parsing of data.
+            onComplete(.sucess(reviewRequestResponse.data))
+        }.resume()
+    }
+}
