3fdd2be7eafa98ba3d723492467ccfb406f46444
Furqan Khan <furqan@freeletics.com>
2019-05-25 13:29:52 +0200

    Created cell for reviews

---
+++ Tempelhof Reviews/Reviews/Cell/ReviewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewCell.swift
+//  Tempelhof Reviews
+//
+//  Created by Furqan on 2019-05-25.
+//  Copyright © 2019 Furqan. All rights reserved.
+//
+
+import UIKit
+
+class ReviewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
